0209d6e2282ba302836478de2426319e35994d07
Natalie McCroy <nataliemccroy@gmail.com>
2019-05-29 20:35:44 -0500

    completed adding card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => (
+	<div>
+		<h6>Get started with React</h6>
+		<p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+		<p>reactjs.org</p>
+	</div>
+);
+
+export default CardContent;
